9df0c19319b0d0de1f6477114255a994bc2b38c7
Melania Balla <melania.balla@nokia.com>
2021-12-04 14:16:05 +0200

    Save work progress

--- nodedocker_app/index.js
+++ nodedocker_app/index.js
@@ -8,23 +8,57 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 
+console.log('=================Mela: trying to connect to Mongo');
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// const options = {
+//   autoIndex: false, // Don't build indexes
+//   reconnectTries: 30, // Retry up to 30 times
+//   reconnectInterval: 500, // Reconnect every 500ms
+//   poolSize: 10, // Maintain up to 10 socket connections
+//   // If not connected, return errors immediately rather than waiting for reconnect
+//   bufferMaxEntries: 0
+// }
+
+// const connectWithRetry = () => {
+//   console.log('===Mela: MongoDB connection with retry')
+//   mongoose.connect("mongodb://mongo:27017/docker-node-mongo", options).then(()=>{
+//     console.log('===Mela: MongoDB is connected')
+//   }).catch(err=>{
+//     console.log('===Mela: MongoDB connection unsuccessful, retry after 5 seconds.')
+//     setTimeout(connectWithRetry, 5000)
+//   })
+// }
+
+// connectWithRetry()
+
+const Item = require('./models/Item');
+
 app.get('/', (req, res) => {
     Item.find()
       .then(items => res.render('index', { items }))
       .catch(err => res.status(404).json({ msg: 'No items found' }));
   });
   
-  app.post('/item/add', (req, res) => {
+app.post('/item/add', (req, res) => {
     const newItem = new Item({
       name: req.body.name
     });
   
     newItem.save().then(item => res.redirect('/'));
-  });
+});
   
-  const port = 3000;
+const port = 3000;
   
-  app.listen(port, () => console.log('Server running...'));
+app.listen(port, () => console.log('=====Mela: Server running...'));
 
 // const server = hapi.server({
 //     port: 3000,
@@ -48,11 +82,3 @@ app.get('/', (req, res) => {
 // server.start();
 // console.log('Server running on %s', server.info.uri);
 
-// Connect to MongoDB
-mongoose
-  .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
