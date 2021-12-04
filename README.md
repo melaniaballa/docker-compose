de683c35ce2884fac34509c139c1670ee78ef300
Melania Balla <melania.balla@nokia.com>
2021-12-04 12:02:47 +0200

    Create needed basic files - mention express was used instead of hapijs, will change later

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docker-compose
 My first push
+
+1. Application started with express and later will switch to hapijs
---
+++ index.js
@@ -0,0 +1,58 @@
+// const hapi = require('@hapi/hapi');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    Item.find()
+      .then(items => res.render('index', { items }))
+      .catch(err => res.status(404).json({ msg: 'No items found' }));
+  });
+  
+  app.post('/item/add', (req, res) => {
+    const newItem = new Item({
+      name: req.body.name
+    });
+  
+    newItem.save().then(item => res.redirect('/'));
+  });
+  
+  const port = 3000;
+  
+  app.listen(port, () => console.log('Server running...'));
+
+// const server = hapi.server({
+//     port: 3000,
+//     host: 'localhost'
+// });
+
+// const start = async () => {
+//     await server.start();
+// };
+
+// start();
+
+// server.route({
+//     path: '/',
+//     method: 'GET',
+//     handler: (request, h) => {
+//       return 'Hello, hapi!';
+//     }
+// });
+
+// server.start();
+// console.log('Server running on %s', server.info.uri);
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
